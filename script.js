f9244b88d05c9c67f00733059c5f2c68c028db1c
cartoonartist <cartoonartist10m@gmail.com>
2026-06-13 15:54:49 +0530

    Implement background video toggle functionality
    Add event listener to toggle background video on end

--- script.js
+++ script.js
@@ -370,6 +370,29 @@ window.addEventListener(
     JSON.stringify(wishlist)
     );
 });
+const bgVideo =
+document.getElementById("bgVideo");
+
+let forward = true;
+
+bgVideo.addEventListener("ended", () => {
+
+    if(forward){
+
+        bgVideo.src =
+        "videos/galaxy_reverse.mp4";
+
+    }else{
+
+        bgVideo.src =
+        "videos/galaxy.mp4";
+    }
+
+    forward = !forward;
+
+    bgVideo.load();
+    bgVideo.play();
+});
 function getDrawingPrice(groupName){
 
     const selected =
