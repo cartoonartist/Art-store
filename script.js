7b3c80690cccafb6a5af1c0a18202665681bb390
cartoonartist <cartoonartist10m@gmail.com>
2026-06-14 11:52:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -512,7 +512,7 @@ async function commissionOrder() {
 // SUBMIT PAYMENT - FORMSUBMIT WITH HTML IMAGES
 // ===========================
 // CHANGE THIS TO YOUR EMAIL ADDRESS
-const YOUR_EMAIL = "cartoonartist10m.com";
+const YOUR_EMAIL = "cartoonartist10m@gmail.com";
 
 const submitPaymentBtn = document.getElementById("submitPayment");
 if(submitPaymentBtn) {
